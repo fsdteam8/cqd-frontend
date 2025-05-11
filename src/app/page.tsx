d6a46c613dc4a4e1f17f0422f3ccf3f1d0746a3f
ILhankhondaker <ilhankhondaker@gmail.com>
2025-05-11 20:54:55 +0600

    add why chooseus

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import About from "@/components/About";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">C Q D</h1>
       <About />
+      <WhyChooseUs />
     </div>
   );
 }
