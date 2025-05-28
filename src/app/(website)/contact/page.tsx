5c4c67f033ebbb055043c65003f3dc4c91b34a31
Mehedi Hasan Shishir <154676953+mehedishishir0@users.noreply.github.com>
2025-05-28 16:32:16 +0600

    added

---
+++ src/app/(website)/contact/page.tsx
@@ -0,0 +1,12 @@
+import ContactSection from "@/components/Contact";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <ContactSection />
+    </div>
+  );
+};
+
+export default page;
